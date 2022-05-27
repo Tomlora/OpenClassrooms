4720fb48fca01cb45214a34d2d3d10262402dd87
Kévin <101778157+Tomlora@users.noreply.github.com>
2022-05-27 21:18:29 +0200

    Add files via upload

--- fonctions_personnelles.py
+++ fonctions_personnelles.py
@@ -12,6 +12,7 @@ import sklearn.metrics
 from xgboost import XGBRegressor
 from sklearn import svm
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 ########################## - DataFrame
 
@@ -447,4 +448,34 @@ def feature_importance(model, classifier:str, index:list):
     return fig
 
 
+############################ Corrélation
 
+
+
+# Src : https://medium.com/@szabo.bibor/how-to-create-a-seaborn-correlation-heatmap-in-python-834c0686b88e
+
+def correlation_independante_et_dependante(df:pd.DataFrame, colonne:str, title:str):
+        
+        """Corrélation entre des variables indépendantes avec une variable dépendante.
+        Renvoie une heatmap en une seule colonne
+
+    Parameters
+    -----------
+    df: :class:`DataFrame`
+            Le DataFrame
+    colonne: :class:`str`
+            La variable dépendante, où on va juger les corrélations des autres variables
+    title: :class:`str`
+            Titre du graphique
+            
+    Return
+    ----------
+    Graphique
+    """
+        
+        f, ax = plt.subplots(figsize=(11, 20))
+
+        heatmap = sns.heatmap(df.corr()[[colonne]].sort_values(by=colonne, ascending=False), annot=True)
+        
+        heatmap.set_title(title, fontdict={'fontsize':18}, pad=16);
+        
